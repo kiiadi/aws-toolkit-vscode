806c0da371923098123e754ce52ea9d973cf6bb6
JadenSimon <sijaden@amazon.com>
2021-07-21 08:50:45 -0700

    Remove 3rd party extension activation from source

--- src/shared/cloudformation/activation.ts
+++ src/shared/cloudformation/activation.ts
@@ -32,10 +32,7 @@ export const TEMPLATE_FILE_EXCLUDE_PATTERN = /.*[/\\]\.aws-sam([/\\].*|$)/
  */
 export async function activate(extensionContext: vscode.ExtensionContext): Promise<void> {
     refreshSchemas(extensionContext)
-    // Note: redhat.vscode-yaml no longer works on vscode 1.42
-    if (await activateExtension(VSCODE_EXTENSION_ID.yaml)) {
-        addCustomTags()
-    }
+    addCustomTags()
 
     try {
         const registry = new CloudFormationTemplateRegistry()
@@ -67,34 +64,34 @@ export async function activate(extensionContext: vscode.ExtensionContext): Promi
 function addCustomTags(): void {
     const currentTags = vscode.workspace.getConfiguration().get<string[]>('yaml.customTags') ?? []
     const cloudFormationTags = [
-        "!And",
-        "!And sequence",
-        "!If",
-        "!If sequence",
-        "!Not",
-        "!Not sequence",
-        "!Equals",
-        "!Equals sequence",
-        "!Or",
-        "!Or sequence",
-        "!FindInMap",
-        "!FindInMap sequence",
-        "!Base64",
-        "!Join",
-        "!Join sequence",
-        "!Cidr",
-        "!Ref",
-        "!Sub",
-        "!Sub sequence",
-        "!GetAtt",
-        "!GetAZs",
-        "!ImportValue",
-        "!ImportValue sequence",
-        "!Select",
-        "!Select sequence",
-        "!Split",
-        "!Split sequence"
+        '!And',
+        '!And sequence',
+        '!If',
+        '!If sequence',
+        '!Not',
+        '!Not sequence',
+        '!Equals',
+        '!Equals sequence',
+        '!Or',
+        '!Or sequence',
+        '!FindInMap',
+        '!FindInMap sequence',
+        '!Base64',
+        '!Join',
+        '!Join sequence',
+        '!Cidr',
+        '!Ref',
+        '!Sub',
+        '!Sub sequence',
+        '!GetAtt',
+        '!GetAZs',
+        '!ImportValue',
+        '!ImportValue sequence',
+        '!Select',
+        '!Select sequence',
+        '!Split',
+        '!Split sequence',
     ]
-    const updateTags = currentTags.concat(cloudFormationTags.filter((item) => currentTags.indexOf(item) < 0))
+    const updateTags = currentTags.concat(cloudFormationTags.filter(item => currentTags.indexOf(item) < 0))
     vscode.workspace.getConfiguration().update('yaml.customTags', updateTags, vscode.ConfigurationTarget.Workspace)
 }
